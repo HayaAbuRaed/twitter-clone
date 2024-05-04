798ef7adbc46b593452cc63c459b14c5932f1b84
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-05-04 19:18:50 +0300

    build app layout structure

--- src/containers/Layout/index.jsx
+++ src/containers/Layout/index.jsx
@@ -1,11 +1,25 @@
+import { Grid, Stack } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+const LayoutGridStyle = {
+  height: "100vh",
+  maxWidth: "1265px",
+};
+
 const Layout = () => {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <Stack alignItems="center">
+      <Grid container sx={LayoutGridStyle}>
+        <Grid item xs={2.346} sx={{ backgroundColor: "red" }}></Grid>
+        <Grid container item xs={9.654} columnGap={4}>
+          <Grid item xs={7} sx={{ backgroundColor: "green" }}>
+            <Outlet />
+          </Grid>
+          <Grid item xs={4.6} sx={{ backgroundColor: "blue" }}></Grid>
+        </Grid>
+      </Grid>
+    </Stack>
   );
 };
 
